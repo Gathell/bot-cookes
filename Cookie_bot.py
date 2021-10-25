cbcd1e3c393b0816a9c6f5ff83c8922f1d2a36d4
Gathell <vladislav.k0707@gmail.com>
2021-10-25 14:18:01 +0300

    new_telegram_bot

---
+++ Cookie_bot.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot('2070685352:AAH0PymFaG2q0ZsfsMBNIFny-2w9kTBKWvM')
+
+@bot.message_handler(content_types=['text','document', 'audio'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
